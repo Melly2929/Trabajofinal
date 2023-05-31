a75e330bbddae53951cda3198b1519806bcf4d59
Rosario <118317818+Melly2929@users.noreply.github.com>
2023-05-31 18:17:13 -0300

    Entrega final
    Revolucion digital

---
+++ javascript.js
@@ -0,0 +1,22 @@
+let edad 
+ 
+  edad= prompt ("Ingrese su edad");
+
+    if(edad >= 18){
+        alert("podes ingresar :)");
+      }
+      else{
+        alert("Entras bajo tu responsabilidad");
+      }
+      
+
+
+
+        
+        function Colabora() {
+          let Colabora = confirm("¿Te gustaria ayudar con la comida y tratamientos de los rescatados?");
+          
+          if ( Colabora == true) { alert ("Copia este link para donar:https://mpago.la/2dExX7g");}
+          else { alert("te invitamos a conocernos");
+        }
+        }
